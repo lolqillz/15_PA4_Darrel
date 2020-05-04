e013b088cfe186ffa34568466759117b0d829c16
UnicornX1 <64125154+UnicornX1@users.noreply.github.com>
2020-05-04 18:51:00 +0800

    Updated Unity Project

---
+++ 15_Darrel_PA4/Assets/Script/UITEXT.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UITEXT : MonoBehaviour
+{
+    int time;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(LoadScene());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    IEnumerator LoadScene()
+    {
+        yield return new WaitForSeconds(1);
+
+        SceneManager.LoadScene("CubeScene");
+    }
+}
